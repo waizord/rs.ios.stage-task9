b15bb07b9e57ccb064d9a8f0c8cdcb524ceacb1a
Ivan <waizord@yandex.ru>
2021-08-05 02:38:07 +0300

    Docs: added custom story and gallery

--- RSSchool_T9/RSSchool_T9/Data/Data.swift
+++ RSSchool_T9/RSSchool_T9/Data/Data.swift
@@ -37,6 +37,8 @@ enum ContentType {
 
 struct FillingData {
     static let data: [ContentType] = [
+        .story(Story(coverImage: .init("story-5"), title: .from("s5-title"), text: .from("s5-text"), paths: [.story1path1, .story1path2, .story1path3])), //pass
+        .gallery(Gallery(coverImage: .init("pepe-0"), title: "Task 9", images: .init(base: "pepe", count: 6))),
         .story(Story(coverImage: .init("story-1"), title: .from("s1-title"), text: .from("s1-text"), paths: [.story1path1, .story1path2, .story1path3])),
         .story(Story(coverImage: .init("story-2"), title: .from("s2-title"), text: .from("s2-text"), paths: [.story2path1, .story2path2])),
         .gallery(Gallery(coverImage: .init("minsk-0"), title: "Minsk", images: .init(base: "minsk", count: 6))),
@@ -44,7 +46,9 @@ struct FillingData {
         .story(Story(coverImage: .init("story-4"), title: .from("s4-title"), text: .from("s4-text"), paths: [.story4path1, .story4path2])),
         .gallery(Gallery(coverImage: .init("code-0"), title: "Code stuff", images: .init(base: "code", count: 10))),
         .gallery(Gallery(coverImage: .init("tesla-0"), title: "Tesla", images: .init(base: "tesla", count: 8))),
-        .story(Story(coverImage: .init("story-3"), title: .from("s3-title"), text: .from("s3-text"), paths: [.story3path1, .story3path1, .story3path1, .story3path1]))
+        .story(Story(coverImage: .init("story-3"), title: .from("s3-title"), text: .from("s3-text"), paths: [.story3path1, .story3path1, .story3path1, .story3path1])),
+        .story(Story(coverImage: .init("story-6"), title: .from("s6-title"), text: .from("s6-text"), paths: [.story1path1, .story1path2, .story1path3])), //pass
+        .gallery(Gallery(coverImage: .init("anime-0"), title: "Anime", images: .init(base: "anime", count: 4)))
     ]
 }
 
---
+++ RSSchool_T9/RSSchool_T9/Data/Static/story-6/story-6-path.swift
@@ -0,0 +1,10 @@
+//
+// 📰 🐸 
+// Project: RSSchool_T9
+// 
+// Author: Ivan
+// On: 8/4/21
+// 
+// Copyright © 2021 RSSchool. All rights reserved.
+
+import Foundation
